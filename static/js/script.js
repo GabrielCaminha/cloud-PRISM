d7215b53ba6bdecc863afce5e90d7febba17647d
enio ferreira <e.nioferreira@hotmail.com>
2024-07-24 20:11:09 -0300

    front-end init

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,9 @@
-document.getElementById('modelForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const model = document.getElementById('model').value;
-    const properties = document.getElementById('properties').value;
-    const constants = document.getElementById('constants').value;
-    
+
+function teste(){
+    const model = editormodel.getValue()
+    const properties = editorprop.getValue()
+    const constants = document.getElementById("constants").value
+
     const data = {
         model: model,
         properties: properties,
@@ -20,22 +19,45 @@ document.getElementById('modelForm').addEventListener('submit', function(event) 
     })
     .then(response => response.json())
     .then(data => {
-        const resultDiv = document.getElementById('result');
-        resultDiv.innerHTML = `
-            <h2>PRISM Result</h2>
-            <p><strong>Header:</strong> ${data.result.header}</p>
-            <p><strong>Version:</strong> ${data.result.version}</p>
-            <p><strong>Date:</strong> ${data.result.date}</p>
-            <p><strong>Hostname:</strong> ${data.result.hostname}</p>
-            <p><strong>Memory Limits:</strong> ${data.result.memory_limits}</p>
-            <p><strong>Command Line:</strong> ${data.result.command_line}</p>
-            <h3>Model File</h3>
-            <pre>${data.result.model_file}</pre>
-            <h3>Properties File</h3>
-            <pre>${data.result.properties_file}</pre>
-            <h3>Simulation</h3>
-            <pre>${data.result.simulation}</pre>
-        `;
+        if (data.error) {
+            editorterminal.setValue('Error: ' + data.error);
+        } else {
+            editorterminal.setValue(data.result)
+        }
     })
     .catch(error => console.error('Error:', error));
-});
+}
+
+/*
+document.getElementById('modelForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const model = document.getElementById('editor').value;
+    const properties = document.getElementById('editor1').value;
+    const constants = document.getElementById('constants').value;
+    
+    const data = {
+        model: model,
+        properties: properties,
+        constants: constants
+    };
+    console.log(data)
+
+
+    fetch('/process', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.error) {
+            document.getElementById('result').textContent = 'Error: ' + data.error;
+        } else {
+            document.getElementById('result').textContent = data.result;
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});*/
