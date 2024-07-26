ffd73043d8a8d0549ea94779c80606f55eae68f0
enio ferreira <e.nioferreira@hotmail.com>
2024-07-26 14:40:25 -0300

    fixfrontend

--- static/js/script.js
+++ static/js/script.js
@@ -53,37 +53,3 @@ function changeMode(){
 
     darkmode = !darkmode
 }
-
-/*
-document.getElementById('modelForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const model = document.getElementById('editor').value;
-    const properties = document.getElementById('editor1').value;
-    const constants = document.getElementById('constants').value;
-    
-    const data = {
-        model: model,
-        properties: properties,
-        constants: constants
-    };
-    console.log(data)
-
-
-    fetch('/process', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            document.getElementById('result').textContent = 'Error: ' + data.error;
-        } else {
-            document.getElementById('result').textContent = data.result;
-        }
-    })
-    .catch(error => console.error('Error:', error));
-});*/
